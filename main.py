c981ad82fde6a44b0fb40ca827e5f4a036aef9ae
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2020-08-30 16:25:41 -0300

    Initializing genetic algorithim and importing the configuration file

--- main.py
+++ main.py
@@ -237,3 +237,22 @@ def main():
 
 
 main()
+
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    population = neat.Population(config)
+
+    population.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    population.add_reporter(stats)
+
+    winner = population.run('a', 50)
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
